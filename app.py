7f2785fa1cb38caf17fc5cf66b70ef9a5a17f31c
SamiAtte <51129384+SamiAtte@users.noreply.github.com>
2026-09-19 18:38:40 +0300

    Sivun ulkoasun alustava luonnostelu.

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect("/front_page")
 
+@app.route("/front_page")
+def form():
+    return render_template("page.html", front_page=True)
+
+
+#@app.route("/result", methods=["POST"])
+#def result():
+#    message = request.form["message"]
+#    return render_template("result.html", message=message)
+
+#@app.route("/order")
+#def order():
+#    return render_template("order.html")
+
+#@app.route("/order_result", methods=["POST"])
+#def order_result():
+#    pizza = request.form["pizza"]
+#    extras = request.form.getlist("extra")
+#    message = request.form["message"]
+#    return render_template("result.html", pizza=pizza, extras=extras, message=message)
+    
